df19d65bf600bc073b3cba0cd70498b459bdd162
Daiane <daiane.corrente@gmail.com>
2026-04-24 22:35:02 -0300

    novas funções

--- index.js
+++ index.js
@@ -21,8 +21,28 @@ function listarCampanhas() {
   });
 }
 
+function atualizarStatus(id,status) {
+    let campanha=campanhas.find(c=> c.id === id);
+    if (campanha) {
+        campanha.status = status;
+    } else {
+        console.log("Campanha não encontrada");
+    }
+}
+function buscarCampanha(nome) {
+    let campanha=campanhas.find(c=> c.nome === nome)
+    if (campanha) {
+        console.log(campanha.nome)
+    }
+}
 // Testando
 criarCampanha("Black Friday", "Promoção de novembro");
 criarCampanha("Lançamento Produto", "Campanha de lançamento");
 
 listarCampanhas();
+atualizarStatus(3,"inativo");
+listarCampanhas();
+buscarCampanha("Black Friday");
+
+
+
